9a5c91789b4388307defbdb240694a5b1c0e0ae4
Essential Roleplay <yonii10x@gmail.com>
2023-10-02 13:06:52 -0400

    small push

---
+++ [Clothes]/[PDClothing]/[Becca]/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'mp_f_freemode_01_essential.meta',
+    'mp_f_freemode_01_essential2.meta',
+    'mp_m_freemode_01_essential.meta',
+ 
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_essential.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_essential2.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_essential.meta'
+
+
